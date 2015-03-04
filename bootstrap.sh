401276f568fc2909992face533db058fa7507480
Scott Robinson <scott@quadhome.com>
2015-03-04 17:41:00 +0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-sudo sh -c "`curl https://babushka.me/up/hard`"
+sudo sh -c "`curl https://babushka.me/up`"
 babushka sources -a vpn-bastion https://github.com/quad/vpn-bastion.git
 babushka vpn-bastion:provision
