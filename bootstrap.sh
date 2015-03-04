ad177c4d4c65fb70109d5f7be5618fa29deec4d1
sr@thoughtworks.com <sr@thoughtworks.com>
2015-03-04 11:05:11 +0800

    Don't ask me questions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-sudo sh -c "`curl https://babushka.me/up`"
+sudo sh -c "`curl https://babushka.me/up/hard`"
 babushka sources -a vpn-bastion https://github.com/quad/vpn-bastion.git
 babushka vpn-bastion:provision
